5f45ce1b3322626a378409b86c3e2751fffaad06
Brandon <bcolombo@hawk.illinoistech.edu>
2026-04-08 17:02:03 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,29 +1,21 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var input = document.createElement("input");
-    var button = document.createElement("button");
-    var output = document.createElement("h2");
+function start() {
+    var button = document.getElementById("entrybutton");
 
-    input.placeholder = "Type Something...";
-    button.textContent = "Submit";
-    output.textContent = "Your text will appear here";
-
-    document.body.appendChild(input);
-    document.body.appendChild(button);
-    document.body.appendChild(output);
-
-    button.onclick = function() {
-        var text = input.value;
-        if (text === "") {
+    function showText() {
+        var input = document.getElementById("entryinput");
+        var output = document.getElementById("textoutput");
+        
+        if (input.value === "") {
             alert("Please type something first!");
             return;
         }
-    
-    alert("Brandon Colombo: " + text);
-    output.textContent = text;
-    input.value = "";
-    };
-});
+        alert("Brandon Colombo: " + input.value);
+        output.innerHTML = input.value;
+    }
 
+    button.addEventListener("click", showText);
+}
+window.addEventListener("load", start);
 
 
 
